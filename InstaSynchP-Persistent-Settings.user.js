d783457c24a22a278d9e428487409c5f5e105f4b
Julian Hangstörfer <jhangstoerfer@gmail.com>
2015-02-23 23:05:51 +0100

    Refactoring and added comments. Resolves #1

--- InstaSynchP-Persistent-Settings.user.js
+++ InstaSynchP-Persistent-Settings.user.js
@@ -3,7 +3,7 @@
 // @namespace   InstaSynchP
 // @description Makes the InstaSync settings persistent
 
-// @version     1
+// @version     1.0.1
 // @author      Zod-
 // @source      https://github.com/Zod-/InstaSynchP-Persistent-Settings
 // @license     MIT
@@ -32,7 +32,7 @@ function PersistentSettings(version) {
     'type': 'checkbox',
     'default': false,
     'section': ['InstaSync']
-  },{
+  }, {
     'label': 'Show greynames in chat',
     'id': 'instasync-greynames-chat',
     'type': 'checkbox',
@@ -47,58 +47,21 @@ function PersistentSettings(version) {
   }];
 }
 
-PersistentSettings.prototype.autosync = function () {
+PersistentSettings.prototype.load = function (setting, variable, selector, onSettingChange) {
   "use strict";
-  var th = this;
-  window.room.autosync = gmc.get('instasync-autosync');
-  $("#toggle_autosync_box").off();
-  if (!gmc.get('instasync-autosync')) {
-    $("#toggle_autosync_box").trigger('click');
-  }
-  $("#toggle_autosync_box").on("change", function () {
-    gmc.set('instasync-autosync', $(this).is(":checked"));
+  var th = this,
+    set = gmc.get(setting);
+  onSettingChange = onSettingChange || function () {};
+  window.room[variable] = set;
+  //turn off default event, set the checkbox and add our own listener
+  $(selector).off().attr('checked', set).on("change", function () {
+    gmc.set(setting, $(this).is(":checked"));
     window.plugins.settings.save();
   });
-  events.on(th, 'SettingChange[instasync-autosync]', function (ignore, newVal) {
-    window.room.autosync = newVal;
-    if (newVal) {
-      sendcmd('resynch');
-    }
-  });
-};
-
-PersistentSettings.prototype.ytControls = function () {
-  "use strict";
-  var th = this;
-  window.room.showYTcontrols = gmc.get('instasync-yt-controls');
-  $("#toggleYTcontrols_box").off();
-  if (gmc.get('instasync-yt-controls')) {
-    $("#toggleYTcontrols_box").trigger('click');
-  }
-  $("#toggleYTcontrols_box").on("change", function () {
-    gmc.set('instasync-yt-controls', $(this).is(":checked"));
-    window.plugins.settings.save();
-  });
-  events.on(th, 'SettingChange[instasync-yt-controls]', function (ignore, newVal) {
-    window.room.showYTcontrols = newVal;
-    reloadPlayer();
-  });
-};
-
-PersistentSettings.prototype.greynameFilter = function () {
-  "use strict";
-  var th = this;
-  window.room.filterGreyname = gmc.get('instasync-yt-controls');
-  $("#toggle_greyname_chat").off();
-  if (!gmc.get('instasync-greynames-chat')) {
-    $("#toggle_greyname_chat").trigger('click');
-  }
-  $("#toggle_greyname_chat").on("change", function () {
-    gmc.set('instasync-greynames-chat', $(this).is(":checked"));
-    window.plugins.settings.save();
-  });
-  events.on(th, 'SettingChange[instasync-greynames-chat]', function (ignore, newVal) {
-    window.room.filterGreyname = newVal;
+  //set the value when the setting changes
+  events.on(th, 'SettingChange[{0}]'.format(setting), function (ignore, newVal) {
+    window.room[variable] = newVal;
+    onSettingChange(ignore, newVal);
   });
 };
 
@@ -106,36 +69,45 @@ PersistentSettings.prototype.disablePlayer = function () {
   "use strict";
   var th = this;
   window.room.playerDisabled = gmc.get('instasync-disable-player');
+  //remove native events
   $("#disable_player").off();
   $('#reload_btn').off();
-  function save(val){
+
+  function save(val) {
     gmc.set('instasync-disable-player', val);
     window.plugins.settings.save();
   }
+  //kill the player
   $("#disable_player").on("click", function () {
     save(true);
   });
+  //recreate the player on reload
   $('#reload_btn').on("click", function () {
     save(false);
   });
+  //reload/destroy player on save
   events.on(th, 'SettingChange[instasync-disable-player]', function (ignore, newVal) {
-    if(newVal){
+    window.room.playerDisabled = newVal;
+    if (newVal) {
       $("#media").html("");
-    }else{
+    } else {
       reloadPlayer();
     }
-    window.room.playerDisabled = newVal;
   });
 };
 
 PersistentSettings.prototype.preConnect = function () {
   "use strict";
   var th = this;
-  th.autosync();
-  th.greynameFilter();
-  th.ytControls();
+  th.load('instasync-autosync', 'autosync', '#toggle_autosync_box', function (ignore, newVal) {
+    if (newVal) {
+      sendcmd('resynch');
+    }
+  });
+  th.load('instasync-yt-controls', 'showYTcontrols', '#toggleYTcontrols_box', reloadPlayer);
+  th.load('instasync-greynames-chat', 'filterGreyname', '#toggle_greyname_chat');
   th.disablePlayer();
 };
 
 window.plugins = window.plugins || {};
-window.plugins.persistentSettings = new PersistentSettings('1');
+window.plugins.persistentSettings = new PersistentSettings('1.0.1');
