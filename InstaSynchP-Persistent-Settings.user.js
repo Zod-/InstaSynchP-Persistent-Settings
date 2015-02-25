7b314dabf0ccd735249f3cf79fad127db06fa4a0
Julian Hangstörfer <jhangstoerfer@gmail.com>
2015-02-25 21:41:12 +0100

    Setting/checkox has to be inverted

--- InstaSynchP-Persistent-Settings.user.js
+++ InstaSynchP-Persistent-Settings.user.js
@@ -3,7 +3,7 @@
 // @namespace   InstaSynchP
 // @description Makes the InstaSync settings persistent
 
-// @version     1.0.1
+// @version     1.0.2
 // @author      Zod-
 // @source      https://github.com/Zod-/InstaSynchP-Persistent-Settings
 // @license     MIT
@@ -47,15 +47,16 @@ function PersistentSettings(version) {
   }];
 }
 
-PersistentSettings.prototype.load = function (setting, variable, selector, onSettingChange) {
+PersistentSettings.prototype.load = function (setting, variable, selector, neg, onSettingChange) {
   "use strict";
   var th = this,
     set = gmc.get(setting);
   onSettingChange = onSettingChange || function () {};
   window.room[variable] = set;
   //turn off default event, set the checkbox and add our own listener
-  $(selector).off().attr('checked', set).on("change", function () {
-    gmc.set(setting, $(this).is(":checked"));
+  $(selector).off().attr('checked', neg ? !set : set).on("change", function () {
+    var val = $(this).is(":checked");
+    gmc.set(setting, neg ? !val : val);
     window.plugins.settings.save();
   });
   //set the value when the setting changes
@@ -74,10 +75,10 @@ PersistentSettings.prototype.disablePlayer = function () {
   $('#reload_btn').off();
 
   function save(val) {
-    gmc.set('instasync-disable-player', val);
-    window.plugins.settings.save();
-  }
-  //kill the player
+      gmc.set('instasync-disable-player', val);
+      window.plugins.settings.save();
+    }
+    //kill the player
   $("#disable_player").on("click", function () {
     save(true);
   });
@@ -99,15 +100,15 @@ PersistentSettings.prototype.disablePlayer = function () {
 PersistentSettings.prototype.preConnect = function () {
   "use strict";
   var th = this;
-  th.load('instasync-autosync', 'autosync', '#toggle_autosync_box', function (ignore, newVal) {
+  th.load('instasync-autosync', 'autosync', '#toggle_autosync_box', false, function (ignore, newVal) {
     if (newVal) {
       sendcmd('resynch');
     }
   });
-  th.load('instasync-yt-controls', 'showYTcontrols', '#toggleYTcontrols_box', reloadPlayer);
-  th.load('instasync-greynames-chat', 'filterGreyname', '#toggle_greyname_chat');
+  th.load('instasync-yt-controls', 'showYTcontrols', '#toggleYTcontrols_box', false, reloadPlayer);
+  th.load('instasync-greynames-chat', 'filterGreyname', '#toggle_greyname_chat', true);
   th.disablePlayer();
 };
 
 window.plugins = window.plugins || {};
-window.plugins.persistentSettings = new PersistentSettings('1.0.1');
+window.plugins.persistentSettings = new PersistentSettings('1.0.2');
