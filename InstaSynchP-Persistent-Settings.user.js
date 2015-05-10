f915d64e8d906be47c02a8847427e084acf1a371
Julian Hangstörfer <jhangstoerfer@gmail.com>
2015-05-10 20:47:00 +0200

    Changes for the settings library changes Apply settings when script was loading slow

--- InstaSynchP-Persistent-Settings.user.js
+++ InstaSynchP-Persistent-Settings.user.js
@@ -3,7 +3,7 @@
 // @namespace   InstaSynchP
 // @description Makes the InstaSync settings persistent
 
-// @version     1.0.3
+// @version     1.0.4
 // @author      Zod-
 // @source      https://github.com/Zod-/InstaSynchP-Persistent-Settings
 // @license     MIT
@@ -17,84 +17,58 @@
 // ==/UserScript==
 
 function PersistentSettings(version) {
-  "use strict";
+  'use strict';
   this.version = version;
   this.name = 'InstaSynchP Persistent Settings';
   this.settings = [{
-    'label': 'Autosync',
-    'id': 'instasync-autosync',
-    'type': 'checkbox',
+    label: 'Autosync',
+    id: 'instasync-autosync',
+    type: 'checkbox',
+    destination: 'playlist',
     'default': true,
-    'section': ['InstaSync']
+    section: ['InstaSync']
   }, {
-    'label': 'Native YouTube controls',
-    'id': 'instasync-yt-controls',
-    'type': 'checkbox',
+    label: 'Native YouTube controls',
+    id: 'instasync-yt-controls',
+    type: 'checkbox',
+    destination: 'playlist',
     'default': false,
-    'section': ['InstaSync']
+    section: ['InstaSync']
   }, {
-    'label': 'Show greynames in chat',
-    'id': 'instasync-greynames-chat',
-    'type': 'checkbox',
-    'default': false,
-    'section': ['InstaSync']
+    label: 'Show greynames in chat',
+    id: 'instasync-greynames-chat',
+    type: 'checkbox',
+    'default': true,
+    section: ['InstaSync']
   }, {
-    'label': 'Disable player',
-    'id': 'instasync-disable-player',
-    'type': 'checkbox',
+    label: 'Disable player',
+    id: 'instasync-disable-player',
+    type: 'checkbox',
+    destination: 'playlist',
     'default': false,
-    'section': ['InstaSync']
+    section: ['InstaSync']
   }];
 }
 
-PersistentSettings.prototype.load = function (setting, variable, selector, neg, onSettingChange) {
-  "use strict";
-  var th = this,
-    set = gmc.get(setting);
-  onSettingChange = onSettingChange || function () {};
-  window.room[variable] = set;
-  //turn off default event, set the checkbox and add our own listener
-  $(selector).off().attr('checked', neg ? !set : set).on("change", function () {
-    var val = $(this).is(":checked");
-    gmc.set(setting, neg ? !val : val);
-    window.plugins.settings.save();
-  });
-  //set the value when the setting changes
-  events.on(th, 'SettingChange[{0}]'.format(setting), function (ignore, newVal) {
-    window.room[variable] = newVal;
-    onSettingChange(ignore, newVal);
-  });
-};
-
-PersistentSettings.prototype.disablePlayer = function () {
-  "use strict";
-  var th = this;
-  window.room.playerDisabled = gmc.get('instasync-disable-player');
-  //remove native events
-  $("#disable_player").off();
-  $('#reload_btn').off();
-
-  function save(val) {
-      gmc.set('instasync-disable-player', val);
-      window.plugins.settings.save();
-    }
-    //kill the player
-  $("#disable_player").on("click", function () {
-    
-    save(true);
-  });
-  //recreate the player on reload
-  $('#reload_btn').on("click", function () {
-    if (!gmc.get('instasync-disable-player')) {
-      reloadPlayer();
-    } else {
-      save(false);
+PersistentSettings.prototype.executeOnce = function () {
+  'use strict';
+  var _this = this;
+  events.on(_this, 'SettingChange[instasync-autosync]', function (ig, v) {
+    window.room.autosync = v;
+    if (v) {
+      sendcmd('resynch');
     }
   });
-  //reload/destroy player on save
-  events.on(th, 'SettingChange[instasync-disable-player]', function (ignore, newVal) {
-    window.room.playerDisabled = newVal;
-    if (newVal) {
+  events.on(_this, 'SettingChange[instasync-yt-controls]', function (ig, v) {
+    window.room.showYTcontrols = v;
+    reloadPlayer();
+  });
+  events.on(_this, 'SettingChange[instasync-greynames-chat]', function (ig, v) {
+    window.room.filterGreyname = v;
+  });
+  events.on(_this, 'SettingChange[instasync-disable-player]', function (ig, v) {
+    window.room.playerDisabled = v;
+    if (v) {
       $("#media").html("");
     } else {
       reloadPlayer();
@@ -102,18 +76,26 @@ PersistentSettings.prototype.disablePlayer = function () {
   });
 };
 
+PersistentSettings.prototype.postConnect = function () {
+  'use strict';
+  window.room.autosync = gmc.get('instasync-autosync');
+  window.room.showYTcontrols = gmc.get('instasync-yt-controls');
+  window.room.filterGreyname = gmc.get('instasync-greynames-chat');
+  window.room.playerDisabled = gmc.get('instasync-disable-player');
+  reloadPlayer();
+};
+
 PersistentSettings.prototype.preConnect = function () {
-  "use strict";
-  var th = this;
-  th.load('instasync-autosync', 'autosync', '#toggle_autosync_box', false, function (ignore, newVal) {
-    if (newVal) {
-      sendcmd('resynch');
+  'use strict';
+  $('#disable_player').remove();
+  $('#reload_btn').off().on('click', function () {
+    if (!gmc.get('instasync-disable-player')) {
+      reloadPlayer();
+    } else {
+      gmc.set('instasync-disable-player', false);
     }
   });
-  th.load('instasync-yt-controls', 'showYTcontrols', '#toggleYTcontrols_box', false, reloadPlayer);
-  th.load('instasync-greynames-chat', 'filterGreyname', '#toggle_greyname_chat', true);
-  th.disablePlayer();
 };
 
 window.plugins = window.plugins || {};
-window.plugins.persistentSettings = new PersistentSettings('1.0.3');
+window.plugins.persistentSettings = new PersistentSettings('1.0.4');
